060bf973238b26c7fbaac79d6e72f7454b5db312
sponkiii <65453504+sponkiii@users.noreply.github.com>
2021-01-12 14:06:14 +0800

    used maequee

--- js/random-quote-generator.js
+++ js/random-quote-generator.js
@@ -1,9 +1,9 @@
 var quotes = [
-    'Do not mind anything that anyone tells you about anyone else. Judge everyone and everything for yourself.',
-    "If you can visualize it, if you can dream it, there's some way to do it.",
-    'Learning never exhausts the mind.',
-    'The journey of a thousand miles begins with one step.',
-    'If you cannot do great things, do small things in a great way.'
+    '"Do not mind anything that anyone tells you about anyone else. Judge everyone and everything for yourself."',
+    '"If you can visualize it, if you can dream it, there\'s some way to do it."',
+    '"Learning never exhausts the mind."',
+    '"The journey of a thousand miles begins with one step."',
+    '"If you cannot do great things, do small things in a great way."'
 ]
 var authorsVar = [
     '~ Henry James','~ Walt Disney','~ Leonardo da Vinci','~ Lao Tzu','~ Napoleon Hill'
